dd961a5b7ec73a9ec9196b551fd94dcce514fe3f
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-08 23:05:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,11 +2,11 @@
 
 bool carattere (char &a) {
   bool segnalibro = false;
- if (( a> 64 && (a<91)){
-    a = a+32;
+ if (( a > 64 && (a < 91)){
+    a = a + 32;
     segnalibro = true;
-  }else if (( a>96 ) && ( a>123)) {
-    a = a-32;
+  }else if (( a > 96 ) && ( a > 123)){
+    a = a - 32;
     segnalibro = true;
     
    }else {
