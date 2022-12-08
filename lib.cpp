cc704f1ccf52783ff5ed290bf062fa45dcc38ee5
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-08 23:35:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,15 @@
 #include "lib.h"
 
-bool carattere (char &a) {
-  bool segnalibro = false;
- if (( a > 64) && (a < 91)){
-    a = a + 32;
-    segnalibro = true;
-  }else if (( a > 96 ) && ( a > 123)){
-    a = a - 32;
-    segnalibro = true;
-    
-   } else {
-    segnalibro = false;
-   } return segnalibro;
-  }
+bool carattere (char&a)  {
+    bool segnalibro = false;
+    if (( a > 64 ) && ( a < 91)) {
+        a = a + 32;
+        segnalibro = true;
+    } else if ( (a > 96) && ( a < 123)){
+        a = a - 32;
+        segnalibro = true;
+
+    }else {
+        segnalibro = false ;
+    } return segnalibro;
+}
