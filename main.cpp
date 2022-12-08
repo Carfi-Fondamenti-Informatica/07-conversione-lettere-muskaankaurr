438db1e3493e5f76b39250953837f0166c98f0a6
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-08 23:02:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char a;
+  cin >> a;
+  bool carattere (char &a);
+  if(carattere (a) ){
+    cout << a << endl;
+  } else {
+    cout << "errore" << endl;
+ }    
   return 0;
 }
